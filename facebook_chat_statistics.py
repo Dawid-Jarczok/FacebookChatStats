1e2479582ec3c03a70b1200a922db4906a5d5684
Dawid-Jarczok <82517250+Dawid-Jarczok@users.noreply.github.com>
2025-01-22 03:08:38 +0100

    Added words sequences statistics

--- facebook_chat_statistics.py
+++ facebook_chat_statistics.py
@@ -702,6 +702,13 @@ class FacebookChatStatistics(FacebookMessengerConversation):
         text += 'Top {} reactions emojis: {}\n'.format(self.nbr_top_emojis, list(self.top_reactions_emojis.keys()))
         text += get_stats(self.emojis_reactions_all_count, sum(self.emojis_reactions_all_count.values()), p_len=self.p_len) + '\n'
 
+        # Words sequences
+        text += '\n' + banner('Words sequences') + '\n'
+        for words_num in [2, 3, 4, 5]:
+            text += 'Top {} words {} seq:\n'.format(10, words_num)
+            for i, (seq, count) in enumerate(list(self.words_sequences[words_num].items())[:10]):
+                text += '{: >4} {} {}\n'.format(f'{i+1}.'.ljust(4), f'({count})'.ljust(6), seq)
+
         # If the text is for terminal, return wihout top words
         if for_terminal:
             return text
